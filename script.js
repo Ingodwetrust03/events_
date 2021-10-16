72eaa0c5e0402274ae5c3cdd37db602c0f396027
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-10-16 20:31:04 +0300

    lesson 10

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let btn = document.getElementById('btn')
+let inputText = document.querySelector('input[type=text')
+let square = document.getElementById('square')
+let inputVal
+let eBtn = document.getElementById('e_btn')
+let inputRange = document.querySelector('input[type=range]')
+let rangeSpan = document.getElementById('range-span')
+
+
+function isNumber (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+inputText.addEventListener('input', function(){
+    inputVal = inputText.value
+})
+
+
+btn.addEventListener('click', function(){
+    if(!isNumber(inputVal)){
+    square.style.backgroundColor = inputVal
+    } else {
+        inputText.value = 'Введите название цвета на англ'
+    }
+})
+
+
+eBtn.style.display = 'none'
+
+inputRange.addEventListener('input', function(){
+    rangeSpan.textContent = event.target.value
+    square.style.height = event.target.value + '%'
+    square.style.width = event.target.value + '%'
+})
+
+
+
